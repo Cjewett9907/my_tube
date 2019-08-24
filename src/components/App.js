c3c4aa4cc17929e0ff2b15bd55454a9770d2d058
Cjewett_work <Craig9907@gmail.com>
2019-08-24 16:53:47 -0700

    update search bar with on change handler

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ class App extends React.Component{
     render(){
 
 
-        return (<div>
-            
+        return (
+            <div className="ui container">
                 <SearchBar />
                 <div>APP!</div>
              </div>
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,13 +2,23 @@ import React from 'react'
 
 
 class SearchBar extends React.Component {
+
+    state = { term: ''};
+
+    handleChange = () => {
+
+    };
+
+
     render(){
         return(
         <div className="search-bar ui segment">
             <form className="ui form">
                 <div className="field">
                     <label>Video Search</label>
-                    <input type="text" />
+                    <input type="text" 
+                    onChange={this.handleChange} 
+                    value={this.state.term} />
 
                 </div>
             </form>
