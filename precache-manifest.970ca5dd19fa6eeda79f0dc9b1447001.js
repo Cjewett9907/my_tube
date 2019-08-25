cb3a86fde9dfca54a4e2a9fb538cc92ea1bb67b7
Cjewett_work <Craig9907@gmail.com>
2019-08-25 05:51:49 -0700

    Updates

---
+++ precache-manifest.970ca5dd19fa6eeda79f0dc9b1447001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d276ce85f915e10cd1a9a3d70f79802",
+    "url": "/my_tube/index.html"
+  },
+  {
+    "revision": "10837cf5ddd2ac97d2ec",
+    "url": "/my_tube/static/css/main.643b2cb8.chunk.css"
+  },
+  {
+    "revision": "8bd38698bc9fd9b46a23",
+    "url": "/my_tube/static/js/2.3e58d82b.chunk.js"
+  },
+  {
+    "revision": "10837cf5ddd2ac97d2ec",
+    "url": "/my_tube/static/js/main.1f0bc6f9.chunk.js"
+  },
+  {
+    "revision": "70533161292572a997df",
+    "url": "/my_tube/static/js/runtime~main.dcca1c7d.js"
+  }
+]);
