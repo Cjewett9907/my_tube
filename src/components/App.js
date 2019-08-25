9fa115f454716e4a76f46bd1b41b16918ec9ff5f
Cjewett_work <Craig9907@gmail.com>
2019-08-24 17:58:51 -0700

    update with youtube requests and valid axios handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import SearchBar from './SearchBar'
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
 
 
 class App extends React.Component{
+    
+    onTermSubmit = (term) => {
+        //  console.log(term)
+        youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+    };
+
     render(){
 
 
         return (
             <div className="ui container">
-                <SearchBar />
+                <SearchBar onTermSubmit={this.onTermSubmit} />
                 <div>APP!</div>
              </div>
             )
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -14,6 +14,7 @@ class SearchBar extends React.Component {
     onFormSubmit = (event) => {
         event.preventDefault();
         // TODO call callback from parent conponent
+        this.props.onTermSubmit(this.state.term);
 
     }
 
